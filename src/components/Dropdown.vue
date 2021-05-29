84ab60d9264a6754689a59972e9e6acc7889f00c
unknown <salar.kafashan@gmail.com>
2021-05-29 10:34:55 -0400

    install vue.js
    start project based on vue.js and install all dependencies like vue cli and bootstrap and etc.

---
+++ src/components/Dropdown.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+     <div>
+        <b-dropdown id="dropdown-1" text="Please choose your favorite topic" class="mb-md-2">
+            <b-dropdown-item>First Action</b-dropdown-item>
+            <b-dropdown-item>Second Action</b-dropdown-item>
+            <b-dropdown-item>Third Action</b-dropdown-item>
+        </b-dropdown>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Dropdown',
+}
+</script>
+
+
---
+++ src/components/Table.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+     <table class="table table-hover text-start">
+        <thead class="table-dark">
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">Topics</th>
+            <th scope="col">Levels</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in items" :key="item.id" scope="row" v-on:click="showDetails(item)">
+            <th>{{item.id}}</th>
+            <td>{{item.topics.toString().replaceAll(",", " - ")}}</td>
+            <td>{{item.levels.toString().replaceAll(",", " - ")}}</td>
+          </tr>
+        </tbody>
+      </table>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+
+export default {
+
+  name: 'Table',
+
+   mounted () {
+    axios.get('https://api.sampleapis.com/codingresources/codingResources')
+    .then(response => (this.items = response.data))
+  },
+  
+   data(){
+    return{
+      items: null
+    }
+  },
+
+  methods: {
+      showDetails: function (item) {
+          alert(item.id);
+      }
+  },
+}
+</script>
+
+
