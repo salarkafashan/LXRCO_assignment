22d4c7913eae9eccd0e069298906b472c63cf727
unknown <salar.kafashan@gmail.com>
2021-05-29 16:00:05 -0400

    project is done
    table,dropdown component added and tested.

--- src/components/Dropdown.vue
+++ src/components/Dropdown.vue
@@ -1,18 +1,49 @@
 <template>
-  <div>
-     <div>
-        <b-dropdown id="dropdown-1" text="Please choose your favorite topic" class="mb-md-2">
-            <b-dropdown-item>First Action</b-dropdown-item>
-            <b-dropdown-item>Second Action</b-dropdown-item>
-            <b-dropdown-item>Third Action</b-dropdown-item>
-        </b-dropdown>
+    <div>
+        <select  v-on:change="selectTopic($event)" class="form-select mb-3" aria-label="Default select example">
+            <option selected disabled>Please choose your favorite topic</option>
+            <option value="java">java</option>
+            <option value="css">css</option>
+            <option value="git">git</option>
+            <option value="rails">rails</option>
+            <option value="sql">sql</option>
+            <option value="angularjs">angularjs</option>
+            <option value="php">php</option>
+            <option value="iOS">iOS</option>
+            <option value="android">android</option>
+            <option value="java">java</option>
+            <option value="c#">c#</option>
+            <option value="game development">game development</option>
+            <option value="databases">databases</option>
+            <option value="wordpress">wordpress</option>
+            <option value="html">html</option>
+            <option value="ruby">ruby</option>
+            <option value="App Development">App Development</option>
+            <option value="computer science">computer science</option>
+            <option value="aws">aws</option>
+            <option value="cloud security">cloud security</option>
+            <option value="bug">bug</option>
+            <option value="iot">iot</option>
+            <option value="smalltalk">smalltalk</option>
+            <option value="misc">misc</option>
+            <option value="Go">Go</option>
+            <option value="security">security</option>
+            <option value="ctf">ctf</option>
+            <option value="learn to code">learn to code</option>
+            <option value="javascript">javascript</option>
+        </select>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  name: 'Dropdown',
+    name: 'Dropdown',
+    methods: {
+        selectTopic: function(value) {
+            // alert(event.target.value);
+            this.$emit('update:option', value);
+        }
+    }
 }
 </script>
 
--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,5 +1,13 @@
 <template>
   <div>
+    <div class="row mt-5">
+        <div class="col-md-3 text-start">
+          <Dropdown @update:option="optionUpdate"/>
+        </div>
+        <div class="col-md-9 text-end">
+          <h5>Welcome to my assignment</h5>
+        </div>
+      </div>
      <table class="table table-hover text-start">
         <thead class="table-dark">
           <tr>
@@ -16,31 +24,76 @@
           </tr>
         </tbody>
       </table>
+
+      <b-modal id="detail" size="lg" centered title="More Detail :" header-bg-variant="dark" header-text-variant="light" bodyTextVariant="dark" ok-only hide-header-close>
+
+        <span class="fw-bold">Description :</span>
+        <p class="my-2">{{description}}</p>
+
+        <span class="fw-bold">Types : </span>
+        <p class="my-2">{{types.toString().replaceAll(",", " - ")}}</p>
+
+        <span class="fw-bold">Topics : </span>
+        <p class="my-2">{{topics.toString().replaceAll(",", " - ")}}</p>
+
+        <span class="fw-bold">Levels :</span>
+        <p class="my-2"> {{levels.toString().replaceAll(",", " - ")}}</p>
+
+        <span class="fw-bold">Url : </span>
+        <br>
+        <a :href=url>{{url}}</a>
+        <br>
+      </b-modal>
   </div>
 </template>
 
 <script>
 import axios from "axios";
-
+import Dropdown from './Dropdown.vue'
 
 export default {
 
   name: 'Table',
-
+  components: {
+    Dropdown,
+  },
    mounted () {
     axios.get('https://api.sampleapis.com/codingresources/codingResources')
-    .then(response => (this.items = response.data))
+    .then(response => (this.items = response.data , this.copyItems = response.data))
   },
   
    data(){
     return{
-      items: null
+      test:         null,
+      items:        null,
+      description:  String,
+      url:          String,
+      types:        Array,
+      topics:       Array,
+      levels:       Array,
     }
   },
 
   methods: {
       showDetails: function (item) {
-          alert(item.id);
+          this.$bvModal.show('detail');
+          this.description = item.description;
+          this.url = item.url;
+          this.types = item.types;
+          this.topics = item.topics;
+          this.levels = item.levels;
+      },
+      optionUpdate:function (event)
+      {
+        var selectedTopics = [];
+        var copyItems = this.copyItems;
+        copyItems.forEach(function(entry) {
+           if(entry.topics.includes(event.target.value))
+           {
+              selectedTopics.push(entry);
+           }
+        });
+        this.items = selectedTopics; 
       }
   },
 }
